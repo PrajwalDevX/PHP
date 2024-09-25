2110135c238a598116fb086ce7d5565d06201b14
PrajwalDevX <ghatoleprajwal31@gmail.com>
2024-09-25 20:26:30 +0530

    Update table_1to10.php

--- table_1to10.php
+++ table_1to10.php
@@ -1,18 +1,41 @@
-<table border="1">
+<html>
+    <head>
+<style>
+
+    #table{
+        border-radius: 12px;
+        background-color:lightsalmon;
+        padding: 12px;
+        border: 2px;
+        margin: 12px;
+        /* border: salmon; */
+    }
+
+
+    #table td{
+        /* border: saddlebrown; */
+        /* border-radius: 12px; */
+        padding: 12px;
+        /* color:aliceblue; */
+    }
+
+
+</style>
+</head>
+</html>
+<table border="1" id="table">
 
 <?php
 
-for($num=1;$num<=10;$num++) { // Outer loop for numbers 1 to 10
-    echo "<tr><th colspan='5'>Multiplication Table for $num</th></tr>"; // Heading for each table
-    for($i=1;$i<=10;$i++) { // Inner loop for each multiplication step
-        echo "<tr>";
-        echo "<td>$num</td>";
-        echo "<td>*</td>";
-        echo "<td>$i</td>";
-        echo "<td>=</td>";
-        echo "<td>".$num*$i."</td>";
-        echo "</tr>";
-    }
+for ($num = 1; $num <= 10; $num++)
+ {
+    echo "<tr>";
+       for ($i = 1; $i <= 10; $i++) 
+{
+        echo "<td>".$num * $i."</td>"; // Multiplication results
+   
+ }
+    echo "</tr>";
 }
 
 ?>
